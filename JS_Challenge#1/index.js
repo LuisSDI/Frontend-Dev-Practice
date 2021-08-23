ba70e6d87ebb82c97c7e833c12c3bc8e32a837d3
Luis Altamirano Urovo <luisjoseaj3@gmail.com>
2021-08-23 18:07:59 +0800

    v.2  Final Calculations

--- JS_Challenge#1/index.js
+++ JS_Challenge#1/index.js
@@ -34,6 +34,8 @@ function calculateCinema(){
     let movies = Number(document.getElementById("cinema_num").innerHTML);
     let price = Number(document.getElementById("cinema_cost").value);
     let total = document.getElementById("cinema_total");
+    let pricePerMovie = document.getElementById("cinema_per_movie");
+    pricePerMovie.innerHTML = price;
     total.innerHTML = `${movies * price}`;
 }
 
@@ -81,10 +83,25 @@ function moviesLeft(){
 
 function streamTotal(){
     let streamCost = document.querySelectorAll('*[id^="stream_cost"]');
+    let streamPerMovie = document.querySelectorAll('*[id^="stream_total"]');
+    let moviecost = 0;
     let cost = 0;
+    let activeService = 0;
+    let streamPerMovieText= document.getElementById('stream_per_movie');
+    let total = document.getElementById('stream_final');
+    
     streamCost.forEach(element => {
          cost +=Number(element.value);
       });
-    let total = document.getElementById('stream_final');
+
+      streamPerMovie.forEach(element => {
+        moviecost +=Number(element.innerHTML);
+        if(Number(element.innerHTML) > 0){
+            activeService+=1;
+        }
+     });
+    
+    moviecost = moviecost / activeService;
     total.innerHTML = cost
+     streamPerMovieText.innerHTML = moviecost;
 }
